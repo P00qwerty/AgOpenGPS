22908f82343435ff96f10bb40daca29a8be880cf
p00qwerty Gamer <p00qwerty@outlook.com>
2024-12-26 11:46:10 +0100

    add branchInfo

---
+++ SourceCode/GPS/Properties/BranchInfo.cs
@@ -0,0 +1,11 @@
+using System;
+
+//this file is automaticaly generated on Pre-build
+namespace AgOpenGPS
+{
+    public static class BuildInfo
+    {
+        public static string BranchName = "automaticIsBeta";
+        public static string CommitHash = "f5526a35";
+    }
+}
